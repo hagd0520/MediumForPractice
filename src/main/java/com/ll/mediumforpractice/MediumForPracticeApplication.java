0a09caf975ac8157d5e3305c64e364432480b955
hagd <hagd0520@gmail.com>
2024-01-08 21:12:51 +0900

    init

---
+++ src/main/java/com/ll/mediumforpractice/MediumForPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.ll.mediumforpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediumForPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediumForPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/mediumforpractice/MediumForPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.mediumforpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediumForPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
