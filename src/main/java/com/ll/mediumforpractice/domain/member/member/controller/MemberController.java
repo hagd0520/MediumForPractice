d256e855c80350a375e27f7d955036d6a893f936
hagd <hagd0520@gmail.com>
2024-01-09 19:37:02 +0900

    feat: MemberController 내 JoinForm 추가, join 추가

--- src/main/java/com/ll/mediumforpractice/domain/member/member/controller/MemberController.java
+++ src/main/java/com/ll/mediumforpractice/domain/member/member/controller/MemberController.java
@@ -1,8 +1,11 @@
 package com.ll.mediumforpractice.domain.member.member.controller;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,5 +15,17 @@ public class MemberController {
     @GetMapping("/join")
     public String showJoin() {
         return "domain/member/member/join";
+    }
+
+    @Getter
+    @Setter
+    public static class JoinForm {
+        public String username;
+        public String password;
+    }
+
+    @PostMapping("/join")
+    public String join(JoinForm joinForm) {
+        return "redirect:/";
     }
 }
