2d89fb33f5046e5a0cd6c4ac9a33172c8d78d3b1
hagd <hagd0520@gmail.com>
2024-01-11 19:38:38 +0900

    feat: Add on NotProd

--- src/main/java/com/ll/mediumforpractice/global/init/NotProd.java
+++ src/main/java/com/ll/mediumforpractice/global/init/NotProd.java
@@ -1,15 +1,24 @@
 package com.ll.mediumforpractice.global.init;
 
+import com.ll.mediumforpractice.domain.member.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
+@RequiredArgsConstructor
 public class NotProd {
+    private final MemberService memberService;
     @Bean
     public ApplicationRunner initNotProd() {
         return args -> {
-            System.out.println("NotProd");
+            memberService.join("system", "system");
+            memberService.join("admin", "admin");
+            memberService.join("user1", "1234");
+            memberService.join("user2", "1234");
         };
     }
 }
