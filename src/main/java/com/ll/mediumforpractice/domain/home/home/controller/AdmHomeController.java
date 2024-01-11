bb7c31378bdce69a4b1a55e64bb39dcb2888b5d4
hagd <hagd0520@gmail.com>
2024-01-11 20:25:12 +0900

    feat: Add AdmHomeController.about

--- src/main/java/com/ll/mediumforpractice/domain/home/home/controller/AdmHomeController.java
+++ src/main/java/com/ll/mediumforpractice/domain/home/home/controller/AdmHomeController.java
@@ -1,6 +1,7 @@
 package com.ll.mediumforpractice.domain.home.home.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/adm")
 @RequiredArgsConstructor
 public class AdmHomeController {
-    @GetMapping
+    @GetMapping("")
+    @PreAuthorize("hasRole('ADMIN')")
     public String showMain() {
         return "domain/home/home/adm/main";
+    }
+
+    @GetMapping("/home/about")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String showAbout() {
+        return "domain/home/home/adm/about";
     }
 }
