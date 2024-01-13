4229e050bdc137d3e9a6271c35e4f469e27db124
hagd <hagd0520@gmail.com>
2024-01-13 20:06:30 +0900

    feat: Add QueryDSL

--- src/main/java/com/ll/mediumforpractice/domain/post/post/repository/PostRepository.java
+++ src/main/java/com/ll/mediumforpractice/domain/post/post/repository/PostRepository.java
@@ -1,17 +1,10 @@
 package com.ll.mediumforpractice.domain.post.post.repository;
 
-import com.ll.mediumforpractice.domain.member.member.entity.Member;
 import com.ll.mediumforpractice.domain.post.post.entity.Post;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
     List<Post> findTop30ByIsPublishedOrderByIdDesc(boolean isPublished);
-
-    Page<Post> findByIsPublishedAndTitleContainingIgnoreCaseOrIsPublishedAndBodyContainingIgnoreCase(boolean isPublished, String kw, boolean isPublished_, String kw_, Pageable pageable);
-
-    Page<Post> findByAuthorAndTitleContainingIgnoreCaseOrAuthorAndBodyContainingIgnoreCase(Member author, String kw, Member author_, String kw_, Pageable pageable);
 }
