2cafa326544238a6f93a9ce296451672f1a52091
hagd <hagd0520@gmail.com>
2024-01-14 17:15:10 +0900

    feat: Add blog

---
+++ src/main/java/com/ll/mediumforpractice/domain/post/blog/controller/BlogController.java
@@ -0,0 +1,50 @@
+package com.ll.mediumforpractice.domain.post.blog.controller;
+
+import com.ll.mediumforpractice.domain.member.member.entity.Member;
+import com.ll.mediumforpractice.domain.member.member.service.MemberService;
+import com.ll.mediumforpractice.domain.post.post.entity.Post;
+import com.ll.mediumforpractice.domain.post.post.service.PostService;
+import com.ll.mediumforpractice.global.exceptions.GlobalException;
+import com.ll.mediumforpractice.global.rq.Rq;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/b")
+@RequiredArgsConstructor
+public class BlogController {
+    private final Rq rq;
+    private final MemberService memberService;
+    private final PostService postService;
+
+    @GetMapping("/{username}")
+    public String showList(
+            @PathVariable String username,
+            @RequestParam(defaultValue = "") String kw,
+            @RequestParam(defaultValue = "1") int page
+    ) {
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("id"));
+        Pageable pageable = PageRequest.of(page - 1, 10, Sort.by(sorts));
+
+        Member blogMember = memberService.findByUsername(username).orElseThrow(() -> new GlobalException("404-1", "존재하지 않는 사용자입니다."));
+
+        Page<Post> postPage = postService.search(blogMember, true, kw, pageable);
+
+        rq.attr("username", username);
+        rq.attr("postPage", postPage);
+
+        return "domain/post/blog/list";
+    }
+}
